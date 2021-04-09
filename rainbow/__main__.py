76437c0a0c212e499fd9bc6847c4b0ca8ef1a4e6
Alphons <alphonsg@protonmail.com>
2021-04-09 09:33:04 +0800

    Expand ArgumentParser description

--- rainbow/__main__.py
+++ rainbow/__main__.py
@@ -9,18 +9,37 @@ import yaml
 
 
 def process_args():
-    parser = ArgumentParser(description='Automated air liquid interface cell '
-                                        'culture analysis using deep optical '
-                                        'flow.')
-    parser.add_argument('root_dir', type=str, help=('root directory to begin '
-                        'performing analysis in'))
-    parser.add_argument('config', type=str, help='YAML configuration file')
+    parser = ArgumentParser(description="""
+                            RAINBOW: Automated air liquid
+                            interface cell culture analysis using
+                            deep optical flow. This tool will process
+                            time lapse image sequences from .nd2 files
+                            or standard image files such as .png
+                            files. Rainbow processes all
+                            compatible files in the provided root
+                            directory and in every
+                            subfolder by default. The results of
+                            the analysis are placed in a
+                            folder that is created in the
+                            same directory as the analyzed
+                            image sequence. The results folder
+                            name begins with the type of image
+                            sequence analyzed in brackets and is
+                            named to easily identify the original
+                            sequence. Rainbow can utilize a GPU
+                            to compute optical flow and can
+                            analyze multiple folders in parallel""")
+    parser.add_argument('root_dir', type=str, help='path to root directory '
+                        'to begin searching for compatible files to process '
+                        'in')
+    parser.add_argument('config', type=str, help='path to YAML configuration '
+                        'file')
     parser.add_argument('--num-workers', type=int, default=-1, help='maximum '
-                        'number of CPUs to utilize (default (-1) uses all '
-                        'CPUs)')
+                        'number of CPUs to utilize for parallel analysis '
+                        '(default (-1) uses all CPUs)')
     parser.add_argument('--non-recursive', dest='recursive',
-                        action='store_false', help='do not process compatible '
-                        'files in root directory subfolders')
+                        action='store_false', help='disable processing of '
+                        'compatible files in root directory subfolders')
     args = parser.parse_args()
 
     return args
