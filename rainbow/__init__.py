9f720af206a23d6b30a96b65f4d3ed6e132f6301
Alphons <alphonsg@protonmail.com>
2021-10-26 17:03:21 +0800

    Removed spaces from filenames of generated files

--- rainbow/__init__.py
+++ rainbow/__init__.py
@@ -4,14 +4,13 @@
 # https://opensource.org/licenses/MIT
 from rainbow.optical_flow.optical_flow import OPTICAL_FLOW_FILENAME
 
-RAW_IMGS_DIR_NAME = 'Raw Images'
-FLOW_IMGS_DIR_NAME = 'Flow Images'
-COMB_IMGS_DIR_NAME = 'Combined Images'
+RAW_IMGS_DIR_NAME = 'Raw_Images'
+FLOW_IMGS_DIR_NAME = 'Flow_Images'
+COMB_IMGS_DIR_NAME = 'Combined_Images'
 METADATA_FILENAME = 'metadata'
 MAG_STATS_FILENME = 'magnitude_stats'
 DIRN_STATS_FILENME = 'direction_stats'
-COMB_IMGS_DIR_NAME = 'Combined Images'
 MAG_SCATTER_FILENAME = 'magnitude_scatter_plot'
 DIRN_SCATTER_FILENAME = 'direction_scatter_plot'
 HEATMAPS_DIR_NAME = 'Heatmaps'
-QUIVER_PLOTS_DIR_NAME = 'Quiver Plots'
+QUIVER_PLOTS_DIR_NAME = 'Quiver_Plots'
