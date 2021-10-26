55a7c3d20a57595e15602464b6fb9d2c8e856025
Alphons <alphonsg@protonmail.com>
2021-10-26 17:17:58 +0800

    Remove PWC-Net

--- rainbow/optical_flow/model_factory.py
+++ rainbow/optical_flow/model_factory.py
@@ -1,12 +1,12 @@
-from .pwc_net import PWCNet
 from .gma import GMA
 
 
 class ModelFactory:
     def get_model(self, model, mdl_cfg, reuse_mdl):
-        if model == 'pwc_net':
-            return PWCNet.get_instance(mdl_cfg, reuse_mdl)
-        elif model == 'gma':
+        """Initializes a supported optical flow model.
+
+        Args:
+        if model == 'gma':
             return GMA.get_instance(mdl_cfg, reuse_mdl)
         else:
             msg = f'Chosen optical flow model ({model}) not supported.'
